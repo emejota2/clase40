c1013a2bc646ffa4eaba15977c0cf7d25b8b8159
emejota2 <emejota02@gmail.com>
2022-08-23 21:46:01 -0300

    make it better

--- db/config.js
+++ db/config.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql')
+const config = require('./db.config')
 
 module.exports.request = (query) => new Promise((res, rej) => {
     const connection = mysql.createConnection({
-        host: 'localhost',
-        port: 8889,
-        user: 'root',
-        password: 'root',
-        database: 'blog'
+        host: process.env.LOCAL ? "localhost" : config.HOST,
+        user: process.env.LOCAL ? "root" : config.USER,
+        password: process.env.LOCAL ? "root" : config.PASSWORD,
+        database: process.env.LOCAL ? "blog" : config.DB
     })
+    
     connection.query(query, (error, data, fields) => {
         if(error) rej(error)
         connection.end((err) => {
---
+++ db/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    USER: "b8013b0d5b5bba",
+    PASSWORD: "1e3f07be",
+    HOST: "us-cdbr-east-06.cleardb.net",
+    DB: "heroku_3d6dfdcc1401288"
+}
